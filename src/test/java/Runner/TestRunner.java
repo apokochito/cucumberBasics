6c691afb8c8d53cb40bdb4998c215ded001626b8
Diana Elena <diana.apolinar@hotmail.com>
2019-11-23 13:50:24 -0600

    Run tests using TestNG

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -2,10 +2,11 @@ package Runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/Features",
 glue = {"Steps","Hooks","Pages","Base"})
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
