39503c29ea16f5aa1e5ebcae3f55c120476ca13f
Diana Elena <diana.apolinar@hotmail.com>
2019-11-24 07:54:58 -0600

    Implementing json and html documentation

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/Features",
-glue = {"Steps","Hooks","Pages","Base"})
+glue = {"Steps","Hooks","Pages","Base"},
+plugin = {"html:target/cucumber-html-report", "json:target/cucumber-json-report.json" } )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
